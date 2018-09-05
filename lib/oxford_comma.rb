190992640f40fab1e297db3ce47a9388af02ac8f
Allison Howard <alliemhow@gmail.com>
2018-09-05 09:31:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
  elsif array.size == 2
    array.join(" and ")
  elsif array.size == 3
-   array[0..2].join(", ") and array.insert(-2, "and")
+   array[0] << [", "] << array[1] << [", and "] << array[2]
  end
 end
