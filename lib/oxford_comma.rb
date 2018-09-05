ac488fb816f51217a30369833f3f9750eb043e9f
Allison Howard <alliemhow@gmail.com>
2018-09-05 08:54:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
  elsif array.size == 2
    array.join(" and ")
  elsif array.size == 3
-   array[0..1].join(", ") + " and " + {array.last}
+   array[0..1].join(", ") + " and " + array.last
  end
 end
