db961cf084e7292d687d645ef25674359e46aab4
Allison Howard <alliemhow@gmail.com>
2018-09-05 09:02:49 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
  elsif array.size == 2
    array.join(" and ")
  elsif array.size == 3
-   array[0..2].join(", ") << "and " << array[3]
+   array[0..2].join(", ") << "and " << array.at[3]
  end
 end
